527f75ef4100a0d7ac81d2e51bfd2b590f15f75b
Andreas Müller <mail@devmount.de>
2024-04-20 08:00:39 +0200

    ➕ Repository commit counts

--- build.sh
+++ build.sh
@@ -36,25 +36,23 @@ GH_REPOS_CREATED=$(python3 -c "import json; print(len(json.loads('$RESULT')['dat
 GH_REPOS_CONTRIBUTED=$(python3 -c "import json; print(json.loads('$RESULT')['data']['viewer']['repositoriesContributedTo']['totalCount'])")
 GH_STARS=$(python3 -c "import json; from functools import reduce; print(reduce(lambda a, c: a + c['stargazers']['totalCount'], json.loads('$RESULT')['data']['viewer']['repositories']['nodes'], 0))")
 
-COMMIT_QUERY_TEMPLATE='_YEAR: contributionsCollection(from: \"YEAR-01-01T00:00:00.000Z\") { totalCommitContributions }'
-REVIEW_QUERY_TEMPLATE='_YEAR: contributionsCollection(from: \"YEAR-01-01T00:00:00.000Z\") { totalPullRequestReviewContributions }'
-COMMIT_QUERY=''
-REVIEW_QUERY=''
-for i in $(seq $GH_YEAR $YEAR);
-do
-  COMMIT_YEAR="${COMMIT_QUERY_TEMPLATE//YEAR/$i}"
-  COMMIT_QUERY="${COMMIT_QUERY} ${COMMIT_YEAR}"
-  REVIEW_YEAR="${REVIEW_QUERY_TEMPLATE//YEAR/$i}"
-  REVIEW_QUERY="${REVIEW_QUERY} ${REVIEW_YEAR}"
+QUERY_TEMPLATE='_YEAR: contributionsCollection(from: \"YEAR-01-01T00:00:00.000Z\") { totalCommitContributions totalPullRequestReviewContributions }'
+QUERY=''
+for i in $(seq $GH_YEAR $YEAR); do
+  QUERY="${QUERY} ${QUERY_TEMPLATE//YEAR/$i}"
 done
-COMMIT_QUERY="{viewer{${COMMIT_QUERY}}}"
-REVIEW_QUERY="{viewer{${REVIEW_QUERY}}}"
-COMMIT_RESULT=$(curl -s -i -H 'Content-Type: application/json' -H "Authorization: bearer $GITHUB_TOKEN" -X POST -d "{\"query\": \"query $COMMIT_QUERY\"}" $GH_API_URL | tail -n 1)
-REVIEW_RESULT=$(curl -s -i -H 'Content-Type: application/json' -H "Authorization: bearer $GITHUB_TOKEN" -X POST -d "{\"query\": \"query $REVIEW_QUERY\"}" $GH_API_URL | tail -n 1)
+QUERY="{viewer{${QUERY}}}"
+RESULT=$(curl -s -i -H 'Content-Type: application/json' -H "Authorization: bearer $GITHUB_TOKEN" -X POST -d "{\"query\": \"query $QUERY\"}" $GH_API_URL | tail -n 1)
 
-GH_COMMITS=$(python3 -c "import json; from functools import reduce; print(reduce(lambda a, c: a + c['totalCommitContributions'], json.loads('$COMMIT_RESULT')['data']['viewer'].values(), 0))")
-GH_PRS_REVIEWED=$(python3 -c "import json; from functools import reduce; print(reduce(lambda a, c: a + c['totalPullRequestReviewContributions'], json.loads('$REVIEW_RESULT')['data']['viewer'].values(), 0))")
+GH_COMMITS=$(python3 -c "import json; from functools import reduce; print(reduce(lambda a, c: a + c['totalCommitContributions'], json.loads('$RESULT')['data']['viewer'].values(), 0))")
+GH_PRS_REVIEWED=$(python3 -c "import json; from functools import reduce; print(reduce(lambda a, c: a + c['totalPullRequestReviewContributions'], json.loads('$RESULT')['data']['viewer'].values(), 0))")
 
+# Build GitHub repository stats
+GH_REPOS_LIST=(${REPOS_GITHUB//,/ })
+GH_REPOS_COMMITS=()
+for r in "${GH_REPOS_LIST[@]}"; do
+  GH_REPOS_COMMITS+=($(curl -s -I -k "https://api.github.com/repos/$r/commits?per_page=1" | sed -n '/^[Ll]ink:/ s/.*"next".*page=\([0-9]*\).*"last".*/\1/p'))
+done
 
 # Create a new file from template
 cp index.template.html index.html
@@ -73,3 +71,7 @@ sed -i \
   -e "s/{{gh_commits}}/$GH_COMMITS/g"\
   -e "s/{{gh_stars}}/$GH_STARS/g"\
   -e "s/{{copyright_year}}/$COPYRIGHT_YEAR/g" index.html
+
+for i in $(seq 1 ${#GH_REPOS_LIST[@]}); do
+  sed -i -e "s/{{gh_repo_${i}_commits}}/${GH_REPOS_COMMITS[((i-1))]}/g" index.html
+done
