ae20a1673dd64e3e92618cd1b45c296aed71f307
Andreas Müller <mail@devmount.de>
2024-04-21 23:04:58 +0200

    ➕ General marker replacements

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ let PROFESSION_AGE=(`date +%s`-`date +%s -d $PROFESSION_SINCE_DATE`)/31536000
 let OPENSOURCE_AGE=(`date +%s`-`date +%s -d $OPENSOURCE_SINCE_DATE`)/31536000
 let COPYRIGHT_YEAR=`date +%Y -d $COPYRIGHT_SINCE_DATE`
 let GH_YEAR=`date +%Y -d $OPENSOURCE_SINCE_DATE`
+FINGERPRINT_PREVIEW="$(echo ${PGP_FINGERPRINT_KEYBASE: -16} | sed 's/.\{4\}/& /g')"
 
 # Build GitHub general stats
 QUERY='{
@@ -77,25 +78,49 @@ cp index.template.html index.html
 # Update file while replacing all markers
 # Replace general markers
 sed -i \
-  -e "s/{{year}}/$YEAR/g" \
-  -e "s/{{age}}/$AGE/g" \
-  -e "s/{{profession_age}}/$PROFESSION_AGE/g" \
-  -e "s/{{opensource_age}}/$OPENSOURCE_AGE/g" \
-  -e "s/{{gh_issues}}/$GH_ISSUES/g" \
-  -e "s/{{gh_prs}}/$GH_PRS/g" \
-  -e "s/{{gh_repos_created}}/$GH_REPOS_CREATED/g" \
-  -e "s/{{gh_repos_contributed}}/$GH_REPOS_CONTRIBUTED/g" \
-  -e "s/{{gh_prs_reviewed}}/$GH_PRS_REVIEWED/g" \
-  -e "s/{{gh_commits}}/$GH_COMMITS/g" \
-  -e "s/{{gh_stars}}/$GH_STARS/g" \
-  -e "s/{{copyright_year}}/$COPYRIGHT_YEAR/g" index.html
+  -e "s|{{user_github}}|$USER_GITHUB|g" \
+  -e "s|{{user_dev}}|$USER_DEV|g" \
+  -e "s|{{user_medium}}|$USER_MEDIUM|g" \
+  -e "s|{{user_mastodon}}|$USER_MASTODON|g" \
+  -e "s|{{user_linkedin}}|$USER_LINKEDIN|g" \
+  -e "s|{{user_xing}}|$USER_XING|g" \
+  -e "s|{{user_stackoverflow}}|$USER_STACKOVERFLOW|g" \
+  -e "s|{{user_keybase}}|$USER_KEYBASE|g" \
+  -e "s|{{user_codepen}}|$USER_CODEPEN|g" \
+  -e "s|{{user_x}}|$USER_X|g" \
+  -e "s|{{user_matrix}}|$USER_MATRIX|g" \
+  -e "s|{{pgp_fingerprint_keybase}}|$PGP_FINGERPRINT_KEYBASE|g" \
+  -e "s|{{email}}|$EMAIL|g" \
+  -e "s|{{url_legal}}|$PAGE_URL_LEGAL|g" \
+  -e "s|{{url_privacy}}|$PAGE_URL_PRIVACY|g" \
+  -e "s|{{url_donation}}|$PAGE_URL_DONATION|g" \
+  -e "s|{{url_sponsoring}}|$PAGE_URL_SPONSORING|g" \
+  index.html
+
+# Replace computed markers
+sed -i \
+  -e "s|{{year}}|$YEAR|g" \
+  -e "s|{{age}}|$AGE|g" \
+  -e "s|{{profession_age}}|$PROFESSION_AGE|g" \
+  -e "s|{{opensource_age}}|$OPENSOURCE_AGE|g" \
+  -e "s|{{gh_issues}}|$GH_ISSUES|g" \
+  -e "s|{{gh_prs}}|$GH_PRS|g" \
+  -e "s|{{gh_repos_created}}|$GH_REPOS_CREATED|g" \
+  -e "s|{{gh_repos_contributed}}|$GH_REPOS_CONTRIBUTED|g" \
+  -e "s|{{gh_prs_reviewed}}|$GH_PRS_REVIEWED|g" \
+  -e "s|{{gh_commits}}|$GH_COMMITS|g" \
+  -e "s|{{gh_stars}}|$GH_STARS|g" \
+  -e "s|{{fingerprint_preview}}|$FINGERPRINT_PREVIEW|g" \
+  -e "s|{{copyright_year}}|$COPYRIGHT_YEAR|g" \
+  index.html
 
 # Replace GitHub repository specific markers
 for i in $(seq 1 ${#GH_REPOS_LIST[@]}); do
   sed -i \
-    -e "s/{{gh_repo_${i}_commits}}/${GH_REPOS_COMMITS[((i-1))]}/g" \
-    -e "s/{{gh_repo_${i}_description}}/${GH_REPOS_DESCRIPTION[((i-1))]}/g" \
-    -e "s/{{gh_repo_${i}_stars}}/${GH_REPOS_STARS[((i-1))]}/g" index.html
+    -e "s|{{gh_repo_${i}_commits}}|${GH_REPOS_COMMITS[((i-1))]}|g" \
+    -e "s|{{gh_repo_${i}_description}}|${GH_REPOS_DESCRIPTION[((i-1))]}|g" \
+    -e "s|{{gh_repo_${i}_stars}}|${GH_REPOS_STARS[((i-1))]}|g" \
+    index.html
 done
 
 # Replace dev.to post specific markers
@@ -103,5 +128,6 @@ for i in $(seq 1 ${#DEV_POSTS_LIST[@]}); do
   sed -i \
     -e "s|{{dev_post_${i}_reactions}}|${DEV_POSTS_REACTIONS[((i-1))]}|g" \
     -e "s|{{dev_post_${i}_comments}}|${DEV_POSTS_COMMENTS[((i-1))]}|g" \
-    -e "s|{{dev_post_${i}_reading_time}}|${DEV_POSTS_READING_TIME[((i-1))]}|g" index.html
+    -e "s|{{dev_post_${i}_reading_time}}|${DEV_POSTS_READING_TIME[((i-1))]}|g" \
+    index.html
 done
